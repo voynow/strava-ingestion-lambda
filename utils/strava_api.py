15fc0c89bbb807132119200b460ddf6db4bb71d6
voynow <voynow99@gmail.com>
2022-12-28 16:38:29 -0500

    Changes required for prod env

--- utils/strava_api.py
+++ utils/strava_api.py
@@ -7,27 +7,7 @@ import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-# from headless_chrome import create_driver
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.chrome.options import Options
-
-
-options = Options()
-options.add_argument('--headless')
-
-
-def create_driver(link=None):
-    """
-    Create chrome driver, get link if available
-    """
-    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-    if link:
-        driver.get(link)
-
-    return driver
+from headless_chrome import create_driver
 
 
 def create_driver_helper():
